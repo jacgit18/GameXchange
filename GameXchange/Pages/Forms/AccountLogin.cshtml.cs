b292b073eb30757ce3724c09ef3a88b87ddb26b0
jac <joshuacarpentier21@gmail.com>
2020-04-07 22:01:11 -0400

    added button

--- GameXchange/Pages/Forms/AccountLogin.cshtml.cs
+++ GameXchange/Pages/Forms/AccountLogin.cshtml.cs
@@ -1,16 +1,42 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using GameXchange.Model;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace GameXchange.Pages.Forms
 {
     public class AccountLoginModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+
+        public AccountLoginModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+
+        public IEnumerable<AccountDbGenerator> AccountDbGenerators { get; set; }
+
+        public async Task OnGet()
+        {
+            AccountDbGenerators = await _db.AccountDbGenerator.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var account = await _db.AccountDbGenerator.FindAsync(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            _db.AccountDbGenerator.Remove(account);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
         }
     }
 }
