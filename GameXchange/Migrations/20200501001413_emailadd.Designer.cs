5c4a6d5aded37f30740eedcfab170e3b2ea3303f
jacgit18 <joshuacarpentier21@gmail.com>
2020-04-30 21:56:32 -0400

    added alot

--- GameXchange/Migrations/20200501001413_emailadd.Designer.cs
+++ GameXchange/Migrations/20200501001413_emailadd.Designer.cs
@@ -3,14 +3,16 @@ using GameXchange.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GameXchange.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200501001413_emailadd")]
+    partial class emailadd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -33,9 +35,6 @@ namespace GameXchange.Migrations
 
                     b.Property<string>("EmailConfirmation")
                         .HasColumnType("nvarchar(250)");
-
-                    b.Property<string>("LoginDate")
-                        .HasColumnType("varchar(30)");
 
                     b.Property<string>("Password")
                         .HasColumnType("varchar(30)");
